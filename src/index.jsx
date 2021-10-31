b606e838243021e079ca91ae1f28ad000b0d71a6
zobyerEvident <zobyer@evidentbd.com>
2021-10-31 12:28:17 +0600

    Initial setup with vite, antd

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// styles
+import "antd/dist/antd.css";
+import "./assets/css/index.css";
+
+// components
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
